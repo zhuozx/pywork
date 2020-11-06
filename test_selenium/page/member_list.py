843e4238a8cbc71f18b55ce344e3d0df742e2ce0
zhuozx <1074834187@qq.com>
2020-11-06 13:21:28 +0800

    提交selenium直播作业一

--- test_selenium/page/member_list.py
+++ test_selenium/page/member_list.py
@@ -16,7 +16,10 @@ class MemberList(BasePage):
 
     # 删除列表第一个成员
     def del_member(self):
+        # 勾选列表第一条
         self.find(By.XPATH, '//*[@id="member_list"]/tr[1]/td[1]').click()
+        # 点击删除按钮
         self.find(By.XPATH, '//*[@class="ww_operationBar"]/a[3]').click()
+        # 点击确定删除
         self.find(By.XPATH, '//*[@id="__dialog__MNDialog__"]/div/div[3]/a[1]').click()
         return True
