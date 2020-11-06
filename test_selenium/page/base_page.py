e2874eadefae1e108d0dcd56001689b0a39833d3
zhuozx <1074834187@qq.com>
2020-11-06 13:10:32 +0800

    提交selenium直播作业一

--- test_selenium/page/base_page.py
+++ test_selenium/page/base_page.py
@@ -13,15 +13,16 @@ class BasePage:
             # 打开一个新的谷歌浏览器窗口
             self.driver = webdriver.Chrome()
             self.driver.maximize_window()
-            self.driver.implicitly_wait(3)
 
             # 复用现有谷歌浏览器窗口
             # chrome_arg = Options()
             # chrome_arg.debugger_address = '127.0.0.1:9222'
             # self.driver = webdriver.Chrome(options=chrome_arg)
-            # self.driver.implicitly_wait(3)
+
+            self.driver.implicitly_wait(3)
         else:
             self.driver = driver
+
         if self._base_url != '':
             self.driver.get(self._base_url)
 
