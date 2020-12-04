63d281775d049e3d7c3d9c3bef225a255d89f650
zhuozx <1074834187@qq.com>
2020-12-04 20:30:29 +0800

    接口测试直播一作业

--- test_requests/test_tag.py
+++ test_requests/test_tag.py
@@ -31,9 +31,18 @@ class TestTag:
         # 使用jsonpath取到所有name字段值，如果测试数据tag_names是其子集，则表示添加成功
         assert set(tag_names) <= set(jsonpath(r.json(), '$..name'))
 
+    @allure.story('测试添加重复标签')
+    @pytest.mark.parametrize('group_name,tag_names', data['normal'])
+    def test_add_fail_exist(self, group_name, tag_names):
+        # 添加标签
+        r = self.tag.add(group_name, tag_names)
+        r = self.tag.add(group_name, tag_names)
+        assert r.status_code == 200
+        assert r.json()['errcode'] == 40071
+
     @allure.story('测试添加超长标签')
     @pytest.mark.parametrize('group_name,tag_names', data['fail'])
-    def test_add_fail(self, group_name, tag_names):
+    def test_add_fail_overlong(self, group_name, tag_names):
         # 添加标签
         r = self.tag.add(group_name, tag_names)
         assert r.status_code == 200
@@ -48,13 +57,16 @@ class TestTag:
     @allure.story('测试用存在的tag_id查询标签')
     def test_list_by_tag(self):
         # 获取所有id节点
-        tag_id = jsonpath(self.tag.list().json(),'$..id')
-        r = self.tag.list(tag_id)
-        assert r.status_code == 200
-        assert r.json()['errcode'] == 0
-
-        # 使用jsonpath取到所有id值，如果获取的结果是测试数据tag_id的子集，则表示成功查询到了标签
-        assert not set(tag_id) - set(jsonpath(r.json(), '$..id'))
+        r = self.tag.list()
+        if r.json()['tag_group']:
+            tag_id = jsonpath(r.json(), '$..id')
+            r = self.tag.list(tag_id)
+            assert r.status_code == 200
+            assert r.json()['errcode'] == 0
+            # 使用jsonpath取到所有id值，如果获取的结果是测试数据tag_id的子集，则表示成功查询到了标签
+            assert not set(tag_id) - set(jsonpath(r.json(), '$..id'))
+        else:
+            print('没有可查询的标签')
 
     @allure.story('测试用不存在的tag_id查询标签')
     def test_list_fail(self):
@@ -74,6 +86,8 @@ class TestTag:
             # 获取标签列表，查看列表中是否没有了已删除的标签
             r = self.tag.list()
             assert not r.json()['tag_group']
+        else:
+            print('没有可删除的标签')
 
     @allure.story('测试删除不存在的标签')
     def test_del_fail(self):
